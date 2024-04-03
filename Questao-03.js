79860eff4fcf0a1018655b87688fb00ad48d8f5d
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-03 15:58:35 -0400

    Questão 05 concluida

--- Questao-03.js
+++ Questao-03.js
@@ -1,6 +1,6 @@
 /*
 Questão 3: Crie um vetor com 10 elementos e remova o último elemento e imprima
-a matriz sem o elemento removido em JavaScript?
+o vetor sem o elemento removido em JavaScript?
 */
 
 let vetor = [1,2,3,4,5,6,7,8,9,10]
